0b31903959f27d8db6f1631c385c43bb77e5c491
Graham Cox <graham@grahamcox.co.uk>
2025-02-05 09:57:38 +0000

    Moved Clippy config to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-#![deny(clippy::all, clippy::pedantic)]
-#![allow(
-    clippy::module_name_repetitions,
-    clippy::unused_async,
-    clippy::unused_self,
-    clippy::needless_pass_by_value, // For all the usage of ToString
-    clippy::ignored_unit_patterns
-)]
-
 #[cfg(feature = "axum")]
 mod axum;
 mod hal;
